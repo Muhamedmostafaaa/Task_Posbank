6dca12c0ac7eda5ba3e35903e2de39c6bd41c55e
dell <muhamed_mostafaa12@hotmail.com>
2022-09-22 15:20:38 +0200

    init

---
+++ lib/app/app.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(
+      const SystemUiOverlayStyle(
+        statusBarColor: Colors.transparent,
+      ),
+    );
+    return MaterialApp(
+
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:task_posbank/app/app.dart';
+
+void main(){
+runApp(const MyApp());
+
+}
+
